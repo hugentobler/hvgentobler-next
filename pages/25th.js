d00442a159dad5182b020d705af52b4ba6f1ac92
Christopher Hugentobler <hugentoblerc@gmail.com>
2020-03-24 23:06:42 +0800

    Migrating 25th page

--- pages/25th.js
+++ pages/25th.js
@@ -10,7 +10,31 @@ export default function TwentyFifth() {
   return (
     <Layout>
       <section className={css.yellow}>
-        <p>This is the about page</p>
+        <p>
+          🇭🇰<br/>
+          Aug 24<sup>th</sup> Sat from 4:00 to 7:00 p.m.<br/>
+          At Lantau - Lower Cheung Sha Beach<br/>
+        </p>
+        <p>
+          🇹🇼<br/>
+          Aug 31<sup>st</sup> Sat from 7:00 to 10:00 p.m.<br/>
+          At Basement Cafe Taipei<br/>
+        </p>
+        <h2>🍺 🏖️ 🧞 🌊</h2>
+        <h2>
+          I'm turning 25<br/>
+          ¼ of a century<br/>
+          life moves pretty fast
+        </h2>
+        <h2>
+          Let's catch up<br/>
+          before I'm back<br/>
+          to Taipei.
+        </h2>
+        <img data-src='images/25th/main.jpg'/>
+        <h2>
+          See you soon!
+        </h2>
       </section>
     </Layout>
   );
