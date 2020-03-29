83c8795b3a50323ff48a78d1d11e5ae08aa9e8d8
Christopher Hugentobler <chris@appedu.co>
2020-03-29 20:08:32 +0800

    Added css config.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withSass = require('@zeit/next-sass');
 module.exports = withSass({
   cssModules: true,
   cssLoaderOptions: {
+    importLoaders: 1,
     localIdentName: '[name]_[local]_[hash:base64:5]'
   }
 })
