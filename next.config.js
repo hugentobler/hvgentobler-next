cdca14c949fd7aa97128ffc84ec4901fab8c8027
Christopher Hugentobler <chris@appedu.co>
2020-06-04 10:02:14 +0800

    Added bundle analyzer.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
-// Next.js configuration
+/* Next bundle analyzer */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/* Next constants */
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
 } = require('next/constants');
 
 // This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
-module.exports = (phase) => {
+const nextConfig = (phase) => {
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   // when `next build` or `npm run build` is used
@@ -25,3 +30,6 @@ module.exports = (phase) => {
     env,
   };
 };
+
+/* Export */
+module.exports = withBundleAnalyzer(nextConfig);
