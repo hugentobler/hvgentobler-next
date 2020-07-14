d4c53325caf631e3f79a18ca3c7b55a03da839ef
Christopher Hugentobler <chris@appedu.co>
2020-07-14 10:26:41 +0800

    Experiment with about layout.

--- pages/about.js
+++ pages/about.js
@@ -27,13 +27,15 @@ export default function About() {
   return (
     <Layout {...frontmatter}>
       <ImageHero>
-        <img
-          src="/images/about/portrait.jpeg"
-          alt=""
-        />
-        <ImageHeroText>
-          <p>Explorer</p>
-        </ImageHeroText>
+        <Wrap>
+          <img
+            src="/images/about/portrait.jpeg"
+            alt=""
+          />
+          <Filter>
+            <p>Explorer</p>
+          </Filter>
+        </Wrap>
       </ImageHero>
     </Layout>
   );
@@ -42,25 +44,53 @@ export default function About() {
 /**
  * STYLED COMPONENTS
  */
+const theme = {
+  contentWidth: '600px',
+  forNotSmall: 'screen and (min-width: 999px)',
+};
+
 const ImageHero = styled.div`
-  margin-left: calc(-1 * var(--space-2));
-  margin-right: calc(-1 * var(--space-2));
+  align-items: center;
+  display: flex;
+  height: 100vH;
+  min-height: 480px;
+  ${'' /* height: calc(100vH - var(--space-2)); */}
+  ${'' /* margin-right: calc(-1 * var(--space-2)); */}
+  @media ${theme.forNotSmall} {
+    margin-left: calc(-1 * var(--space-2));
+    min-height: 720px;
+  }
+
+`;
+
+const Wrap = styled.div`
+  background-color: white;
+  box-sizing: border-box;
+  height: 100%;
+  max-height: 480px;
+  max-width: 360px;
+  padding: var(--space-1);
+  position: relative;
   img {
     object-fit: cover;
-    height: 100vH;
+    height: 100%;
     width: 100%;
+  }
+  @media ${theme.forNotSmall} {
+    max-height: 600px;
+    max-width: 480px;
   }
 `;
 
-const ImageHeroText = styled.div`
+const Filter = styled.div`
   align-items: center;
   background-color: rgba(28, 28, 28, 0.33);
-  bottom: 0;
+  bottom: var(--space-1);
   color: var(--background-color);
   display: flex;
   justify-content: center;
-  left: 0;
-  top: 0;
+  left: var(--space-1);
+  top: var(--space-1);
   position: absolute;
-  right: 0;
+  right: var(--space-1);
 `;
