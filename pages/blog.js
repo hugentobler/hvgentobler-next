609a45a6e171dc662a98ceccd02299c93f046785
Christopher Hugentobler <chris@appedu.co>
2020-07-18 17:58:26 +0800

    Added min blog page.

--- pages/blog.js
+++ pages/blog.js
@@ -6,10 +6,12 @@
 /**
  * MODULES
  */
-import Layout from '../layouts/main';
+import PropTypes from 'prop-types';
 /* Components */
+import Link from 'next/link';
+import Layout from '../layouts/main';
 // eslint-disable-next-line import/no-unresolved
-import { frontMatter as blogPosts } from './**/*.mdx';
+import { frontMatter as blogPosts } from './{2019,2020}/*.mdx';
 
 /**
  * FRONTMATTER
@@ -23,17 +25,48 @@ const frontmatter = {
 };
 
 /**
+ * CUSTOM COMPONENTS
+ */
+const sortedBlogPosts = blogPosts
+  .sort((a, b) => (
+    Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+  ));
+
+const BlogPost = (props) => {
+  const { __resourcePath, title } = props;
+  const slug = __resourcePath.replace('.mdx', '');
+
+  return (
+    <Link href={`/${slug}`}>
+      <a>
+        <h2>{title}</h2>
+      </a>
+    </Link>
+  );
+};
+
+/**
  * DEFAULT EXPORT
  */
 export default function Blog() {
-  console.log(blogPosts)
   return (
     <Layout {...frontmatter}>
       <section>
-        <article>
-          <h1>Blog</h1>
-        </article>
+        <h1>Blog</h1>
+        {
+          sortedBlogPosts.map((e) => (
+            <BlogPost key={e.title} {...e} />
+          ))
+        }
       </section>
     </Layout>
   );
 }
+
+/**
+ * PROPTYPES
+ */
+BlogPost.propTypes = {
+  __resourcePath: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
