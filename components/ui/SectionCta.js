5db2174fca484837dc4ef0de9f8fcfe7d9dbaeb5
Christopher Hugentobler <chris@appedu.co>
2020-07-28 22:41:40 +0800

    Hotfix svg

--- components/ui/SectionCta.js
+++ components/ui/SectionCta.js
@@ -22,7 +22,7 @@ export default function SectionCta(props) {
         <a>
           <Grid>
             {children}
-            <svg aria-hidden="true" focusable="false" data-icon="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <svg aria-hidden="true" focusable="false" data-icon="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="448" height="512">
               <path fill="currentColor" d="M216.464 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887L209.393 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L233.434 36.465c-4.686-4.687-12.284-4.687-16.97 0z" />
             </svg>
           </Grid>
@@ -51,6 +51,10 @@ SectionCta.defaultProps = {
 const Section = styled.section`
   padding: var(--space-6) var(--space-2);
   position: relative;
+  a {
+    display: block;
+    border: none;
+  }
 `;
 
 const Background = styled.div`
@@ -76,6 +80,7 @@ const Grid = styled.div`
     grid-column: 1 / span 2;
     grid-row: 1 / 1;
     max-height: 2.4rem;
+    width: 100%;
   }
   @media ${(props) => props.theme.forMiddle} {
     & > :nth-child(1) {
