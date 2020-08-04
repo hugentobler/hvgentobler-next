bba5cd5c80c339b4090fde7bd757afab81cc4545
Christopher Hugentobler <chris@appedu.co>
2020-08-04 11:05:55 +0800

    Turned portraits into component.

--- pages/about/index.js
+++ pages/about/index.js
@@ -6,10 +6,6 @@
 /**
  * MODULES
  */
-import styled from 'styled-components';
-import { m as motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-import Link from 'next/link';
 /* Components */
 import Layout from '../../layouts/main';
 import TextHero from '../../components/ui/TextHero';
@@ -18,8 +14,7 @@ import SectionHeader from '../../components/ui/SectionHeader';
 import TableList from '../../components/ui/TableList';
 import SectionCta from '../../components/ui/SectionCta';
 import ThreeColumns from '../../components/ui/ThreeColumns';
-import Image from '../../components/ui/Image';
-import { fadeInParent, fadeInChild } from '../../components/ui/Animations';
+import Portraits from '../../components/Portraits';
 
 /**
  * FRONTMATTER
@@ -32,29 +27,17 @@ const frontmatter = {
   colour: 'blue',
 };
 
-const people = [
-  { path: '/about/christopher', name: 'Christopher Hugentobler', location: 'Taipei' },
-  { path: '/about/vincent', name: 'Vincent So', location: 'Hong Kong' },
-  { path: '/about/patrick', name: 'Patrick Yip', location: 'Singapore' },
-];
-
 /**
  * DEFAULT EXPORT
  */
 export default function About() {
-  /* Intersection observer and animations */
-  const [ref, inView] = useInView({
-    threshold: 0.5,
-    triggerOnce: true,
-  });
-
   return (
     <Layout {...frontmatter}>
       <TextHero caption="About">
         We build digital experiences that people want, then distribute them where people are.
       </TextHero>
       <NavBar />
-      <section ref={ref}>
+      <section>
         <SectionHeader>
           <h1>Meet the extension of your in-house team.</h1>
           <div>
@@ -63,36 +46,7 @@ export default function About() {
             </p>
           </div>
         </SectionHeader>
-        <Portraits
-          initial="hidden"
-          animate={inView ? 'visible' : ''}
-          variants={fadeInParent}
-        >
-          {people.map((e) => (
-            <motion.div
-              variants={fadeInChild}
-            >
-              <Link href={e.path}>
-                <a>
-                  <PortraitWrap>
-                    <Image
-                      alt="portrait"
-                      src="/images/about/portrait.png"
-                      h="1200"
-                      w="1200"
-                      lazy
-                      aspect
-                    />
-                  </PortraitWrap>
-                  <PortraitText>
-                    <h3>{e.name}</h3>
-                    <p><span>{e.location}</span></p>
-                  </PortraitText>
-                </a>
-              </Link>
-            </motion.div>
-          ))}
-        </Portraits>
+        <Portraits />
       </section>
       <section>
         <SectionHeader>
@@ -202,72 +156,3 @@ export default function About() {
     </Layout>
   );
 }
-
-/**
-* STYLED COMPONENTS
-*/
-
-const Location = styled.p`
-  bottom: 0;
-  margin-bottom: var(--space-2);
-  position: absolute;
-`;
-
-const Portraits = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(13, 1fr);
-  grid-auto-flow: dense;
-  > div {
-    grid-column: 4 / 13;
-    padding: var(--space-2) var(--space-1);
-    &:hover {
-      background: rgba(255, 255, 255, 0.05);
-    }
-  }
-  a {
-    border: none;
-    display: block;
-    &:hover {
-      opacity: 1;
-    }
-  }
-  @media ${(props) => props.theme.forMiddle} {
-    > div {
-      grid-column-end: span 5;
-    }
-    > div:nth-child(n) {
-      grid-column-start: -6;
-    }
-    > div:nth-child(2n) {
-      grid-column-start: -11;
-    }
-  }
-  @media ${(props) => props.theme.forNotSmall} {
-    > div {
-      grid-column-end: span 4;
-    }
-    > div:nth-child(n+1) {
-      grid-column-start: -5;
-    }
-    > div:nth-child(3n-1) {
-      grid-column-start: -9;
-    }
-    > div:nth-child(3n) {
-      grid-column-start: -13;
-    }
-  }
-`;
-
-const PortraitWrap = styled.div`
-  margin-left: auto;
-  width: 80%;
-`;
-
-const PortraitText = styled.div`
-  h3 {
-    margin-bottom: var(--space-1);
-  }
-  p {
-    margin: 0;
-  }
-`;
